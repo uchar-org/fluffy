9f641e1fdcc9ad64d454087c74a6acccb154391b
3nln <javohirtech@gmail.com>
2026-05-08 01:02:04 +0500

    feat: empty page text use instead of image

--- lib/widgets/layouts/empty_page.dart
+++ lib/widgets/layouts/empty_page.dart
@@ -1,30 +1,30 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class EmptyPage extends StatelessWidget {
-  static const double _width = 400;
   const EmptyPage({super.key});
   @override
   Widget build(BuildContext context) {
-    final width = min(MediaQuery.sizeOf(context).width, EmptyPage._width) / 2;
     final theme = Theme.of(context);
     return Scaffold(
-      // Add invisible appbar to make status bar on Android tablets bright.
       appBar: AppBar(
         automaticallyImplyLeading: false,
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
       extendBodyBehindAppBar: true,
-      body: Container(
-        alignment: Alignment.center,
-        child: Image.asset(
-          'assets/logo_transparent.png',
-          color: theme.colorScheme.surfaceContainerHigh,
-          width: width,
-          height: width,
-          filterQuality: FilterQuality.medium,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 1),
+          decoration: BoxDecoration(
+            color: theme.colorScheme.surfaceContainerHigh,
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: Text(
+            'Select a chat to start messaging',
+            style: theme.textTheme.titleSmall?.copyWith(
+              color: theme.colorScheme.onSurfaceVariant,
+            ),
+          ),
         ),
       ),
     );
